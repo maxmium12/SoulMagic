b678a733eda2a123250942026580e2388ae4517b
sunmilksong <1971370023@qq.com>
2021-01-07 15:11:33 +0800

    Update BlockOre.java

--- src/main/java/com/github/sunmilksong/blocks/BlockOre.java
+++ src/main/java/com/github/sunmilksong/blocks/BlockOre.java
@@ -8,11 +8,8 @@ import com.github.sunmilksong.util.Reference;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
-
-
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
-
 import net.minecraft.block.state.IBlockState;
 
 import net.minecraft.creativetab.CreativeTabs;
